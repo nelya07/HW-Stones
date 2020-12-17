a9192fbbcb52d8ccb29ed5d24b8c1036f485b91b
nelya07 <75816118+nelya07@users.noreply.github.com>
2020-12-17 03:52:12 +0300

    HW Stones

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Mover : MonoBehaviour
+{
+  private Rigidbody _rigidbody;
+  private bool _isFlying = false;
+
+
+  private void Start()
+  {
+    _rigidbody = GetComponent<Rigidbody>();
+  }
+
+  private void Update()
+  {
+    
+    if (Input.GetKey(KeyCode.W))
+    {
+      _isFlying = true;
+    }
+
+    if (_isFlying)
+    {
+      if (transform.position.y < 5)
+      {
+        _rigidbody.AddForce(Vector3.up * _rigidbody.mass * 1.05f);
+      }
+      if (Input.GetKey(KeyCode.Space))
+      {
+        _isFlying = false;
+        _rigidbody.AddForce(Vector3.forward  * 3000f);
+      }
+    }
+
+   
+  }
+}
